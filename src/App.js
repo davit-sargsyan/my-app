3e7fc6bf0e41d68b2f1e9576b757fd9540c77405
Davit <d.sargsyan>
2020-02-13 10:16:54 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { DragDropContext  } from 'react-beautiful-dnd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+import Comp from './components/data'
+import Header from './header'
+
+import './App.css';
+import { throwStatement } from '@babel/types';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toDo: ['poxos', 'pedros', 'kirakos', 'bardughimeos', 'fndik', ''],
+      completed: ['kikos', 'vardan', 'armen', ''],
+    };  
+  }
+   
+  handleChange = (e, index, dataname) => {
+    const arr = [...this.state[dataname]];    
+    arr.splice(index, 1, e.target.value);
+
+    if(index === arr.length-1){
+      arr.push("");
+    } else if(e.target.value === ""){
+      arr.splice(index, 1);
+    }
+
+    this.setState({[dataname]: arr})
+
+  }
+
+  handleDrag = ({ source, destination }) => {
+    const { index: startIndex, droppableId: dragColumn } = source;
+    const { index: endIndex, droppableId: dropColumn } = destination;
+    
+    if(dragColumn === dropColumn){
+
+      const result = [...this.state[dragColumn]];
+      
+      if (endIndex === result.length -1) return;
+
+      const [removed] = result.splice(startIndex, 1);
+      result.splice(endIndex, 0, removed);
+  
+      this.setState(prev => ({ ...prev, [dragColumn]: result }));
+    }
+    else {
+       const dragResult = [...this.state[dragColumn]];
+       const dropResult = [...this.state[dropColumn]];
+
+       if (endIndex === dropResult.length -1) return;
+
+       const [changed] = dragResult.splice(startIndex, 1)
+       dropResult.splice(endIndex, 0, changed )
+       console.log(dragResult)
+       console.log(dropResult)
+       this.setState(prev => ({ ...prev, [dragColumn]: dragResult, [dropColumn]: dropResult }));
+
+    }
+
+    
+  }
+  render() {
+    return (
+    <div>
+      <Header />
+     <div className='flex'>  
+      <DragDropContext onDragEnd={this.handleDrag}>
+
+        <div>
+            <Comp data={this.state.toDo} handleChange={this.handleChange} dataname= 'toDo' />
+        </div>
+
+        <div>
+            <Comp data={this.state.completed} handleChange={this.handleChange} dataname= 'completed' />
+        </div> 
+            
+      </DragDropContext>
+      </div>
     </div>
-  );
-}
+    );
+  }
+};
 
 export default App;
---
+++ src/components/data.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Droppable, Draggable } from 'react-beautiful-dnd';
+
+export default class Comp1 extends React.Component{
+    render () {
+        return (
+            <Droppable droppableId={this.props.dataname}>
+                {(provided) => (
+                    <ol
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                    >
+                        {this.props.data.map((value, index, arr) => (
+                            <Draggable key={index} isDragDisabled={index === arr.length -1 } draggableId={`${this.props.dataname}${index}`} index={index}>
+                                {(provided) => (
+                                    <li 
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                    >
+                                        <input className='noBorders' onChange={(event) => this.props.handleChange(event, index, this.props.dataname)} value={value}/>
+                                    </li>
+                                )}
+                            </Draggable>
+                        ))}
+                    </ol>
+                )}
+            </Droppable>
+        )
+    }
+        
+}
