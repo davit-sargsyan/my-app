775fe9eea3c73f17e7a435a407ca282afcff887b
Davit <d.sargsyan>
2020-02-19 18:49:20 +0400

    commit

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -2,41 +2,41 @@ import React from 'react';
 
 import { Droppable, Draggable } from 'react-beautiful-dnd';
 
-export default class List extends React.PureComponent{
-    render () {
-        return (
-            <Droppable droppableId={this.props.dataName}>
+const List = ({ dataName, data, handleChange }) => {
+
+  return (
+    <Droppable droppableId={dataName}>
+        {provided => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {data.map((value, index, arr) => (
+              <Draggable
+                key={index}
+                isDragDisabled={index === arr.length -1} 
+                draggableId={`${dataName}${index}`}
+                index={index}
+              >
                 {(provided) => (
-                    <ol
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                    >
-                        {this.props.data.map((value, index, arr) => (
-                            <Draggable
-                                key={index}
-                                isDragDisabled={index === arr.length -1} 
-                                draggableId={`${this.props.dataName}${index}`}
-                                index={index}
-                            >
-                                {(provided) => (
-                                    <li 
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                    >
-                                        <input
-                                            className='noBorders'
-                                            onChange={(event) => this.props.handleChange(event, index, this.props.dataName)}
-                                            value={value}
-                                        />
-                                    </li>
-                                )}
-                            </Draggable>
-                        ))}
-                    </ol>
+                  <li 
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <input
+                      className='noBorders'
+                      onChange={(event) => handleChange(event, index, dataName)}
+                      value={value}
+                    />
+                  </li>
                 )}
-            </Droppable>
-        )
-    }
-        
+              </Draggable>
+            ))}
+          </ol>
+        )}
+      </Droppable>
+    )
 }
+
+export default List;
