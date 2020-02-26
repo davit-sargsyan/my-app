2fdd53265ec04c02a1d4f7659c16b6273db78818
Davit <d.sargsyan>
2020-02-26 16:34:11 +0400

    coomit

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 const Header = ({title}) => {
-
-    return(
-        <h1 className='center'>{title}</h1>
-    )
-
+  return(
+    <h1 className='center'>{title}</h1>
+  )
 }
 
 export default Header
