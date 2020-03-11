bd758f3b9830c26a70a12d4bb5629cc0fb66af52
Davit <d.sargsyan>
2020-03-11 15:10:06 +0400

    commit

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-const Header = ({ title }) => {
-  return(
+const Header = ({ title }) => (
     <h1 className='center'>{title}</h1>
   )
-}
+
 
 export default Header;
